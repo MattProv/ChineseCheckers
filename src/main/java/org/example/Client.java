9f4e99b478ea24aea92759cbc6bc608b1493e1e9
Mateusz Prowans <279702@student.pwr.edu.pl>
2024-12-08 00:25:14 +0100

    init project structure

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Client {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from Client!");
+
+    }
+}
---
+++ src/main/java/org/example/Server.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Server {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World from Server!");
+
+    }
+}
